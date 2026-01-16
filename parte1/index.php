788b3cca9fa128a8f33dfb2c33cfe472cb5c1ef0
Vitor Acer5 <vitor.venante@gmail.com>
2026-01-16 20:02:34 -0300

    iniciando a aula pratica

---
+++ parte1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira aula</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello Woooordd <p>";
+
+
+            echo "testando aula prática. (Tô cansado disso já kkkk)";
+        ?>
+    </h1>
+    <p>Iniciando a primeira aula prática</p>
+</body>
+</html>
