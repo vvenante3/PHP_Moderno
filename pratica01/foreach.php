97140e7faf80887e6f0ce5fcbec866d6d638c5c9
Vitor Acer5 <vitor.venante@gmail.com>
2026-01-20 22:41:32 -0300

    práticas diárias

---
+++ pratica01/foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+    $animal = ["elefante","macaco","urso","guepardo","arara"];
+
+    // echo $animal[3] . "<br>";
+
+    foreach ($animal as $animal) {
+        echo $animal . ";". "<br>";
+    }
+
+?>
