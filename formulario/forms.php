8771040759f74a033102bba84926e9723e58c83f
Vitor Acer5 <vitor.venante@gmail.com>
2026-01-26 19:59:26 -0300

    prática - criação de formulário method POST

---
+++ formulario/forms.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário POST</title>
+</head>
+<body>
+    <header>
+        <h3>Formulário - Prática</h3>
+    </header>
+    <div>
+        <form action="interface.php" method="POST">
+            <label>Primeiro número:</label>
+            <input type="number">
+
+            <br><br>
+
+            <label>Expressão Aritimética:</label>
+            <input type="text">
+
+            <br><br>
+
+            <label>Segundo número:</label>
+            <input type="number">
+
+            <br><br>
+
+            <button type="submit">Calcular</button>
+        </form>
+    </div>
+
+
+    
+</body>
+</html>
