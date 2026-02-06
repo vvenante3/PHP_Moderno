e56c0c15e7da07ca15bf426ff73e10474b0433d9
Vitor Acer5 <vitor.venante@gmail.com>
2026-02-05 21:59:01 -0300

    finalizacao desafio 02

--- parte2/desafio002.php
+++ parte2/desafio002.php
@@ -6,14 +6,8 @@
     <title>Desafio 02 - Sorteio de número</title>
 </head>
     <?php
-        if($metodo === 'GET'){
-            if(isset($_GET['botao'])){
-                //gerar número aleatorio
-                    // minimo 0 | maximo 100
-                //guardar em uma variavel 
-            }
-        } else {
-            echo "método é diferente que GET";
+        if(isset($_GET['botao'])){
+                $numero_aleatorio = random_int(0, 100);
         }
     ?>
 <body>
@@ -22,15 +16,18 @@
     </header>
     <br>
     <section>
+        <?php
+            if(isset($numero_aleatorio)){
+                echo $numero_aleatorio;
+            }
+        ?>
+    <p></p>
+    </section>
+    <section>
         <form method="GET">
             <button type="submit" name="botao" value="numero_aleatorio">Lançar Dado</button>
         </form>
     </section>
     <br>
-    <?php
-        // resultado
-    ?>
-
-    
 </body>
 </html>
