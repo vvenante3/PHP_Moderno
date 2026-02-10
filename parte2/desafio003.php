b311909583f4683e23df9e746266f1d382ec88f4
Vitor Acer5 <vitor.venante@gmail.com>
2026-02-10 18:57:26 -0300

    inicio desafio pratico - conversor de moedas

---
+++ parte2/desafio003.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 03 - Conversor de Moeda</title>
+</head>
+    <?php
+        // recebimento e processamento dos dados
+    ?>
+<body>
+    <header>
+        <h3>Conversor de moeda - Dollar </h3>
+    </header>
+    <br>
+    <section>
+        <!-- resultado -->
+    </section>
+    <section>
+        <form method="GET">
+            <label>Valor em R$</label>
+            <input type="number" name="real" id="rs"></input>
+            <p></p>
+            <label>Cotação U$</label>
+            <input type="number" name="dolar" id="us"></input>
+            <p></p>
+            <button type="submit">Calcular</button>
+        </form>
+    </section>
+</body>
+</html>
