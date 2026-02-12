2043e44c98307d2cd6631e904275a5af19e0102b
Vitor Acer5 <vitor.venante@gmail.com>
2026-02-12 20:44:54 -0300

    inicio desafio 3b - conversao moeda avançado

---
+++ parte2/desafio003b.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de Moedas Avançado</title>
+</head>
+<body>
+    <?php
+        // processamento php
+
+    ?>
+    <div>
+        <header>
+            <h3></h3>
+        </header>
+        <br>
+        <section>
+            <form method="">
+                <!-- input's -->
+                 
+            </form>
+        </section>
+        <br>
+        <section>
+                <!-- exibicao do resultado -->
+        </section>
+    </div>
+    
+</body>
+</html>
