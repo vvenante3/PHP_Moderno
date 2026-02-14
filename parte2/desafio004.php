1586b5b59245e99e776788bfe115abf49ac1476b
Vitor Acer5 <vitor.venante@gmail.com>
2026-02-13 21:32:22 -0300

    finalização pratica desafio004

--- parte2/desafio004.php
+++ parte2/desafio004.php
@@ -8,8 +8,11 @@
 <body>
     <div>
         <?php
-        if($_GET['numero']){
+        if($_GET['numero'] ?? 0 ){
             $numero = $_GET['numero'];
+
+            $int = (int) $numero;
+            $fra = $numero - $int;
         }
         ?>
     </div>
@@ -18,14 +21,16 @@
     </header>
     <section>
         <form method="GET">
-            <input type="number" name="numero" id="numero" placeholder="Digite o número" required>
+            <input type="number" name="numero" id="numero" step="0.001" placeholder="Digite o número" required>
             <button type="submit">Analisar</button>
         </form>
     </section>
     <br>
     <section>
         <?php
-            echo $numero;
+            echo "O número digitado pelo usuárip foi: <strong>" . number_format($numero, 3, ",", ".") . "</strong><br>";
+            echo "A parte inteira do número é: " . number_format($int, 0, ",", ".") . "<br>";
+            echo "A parte frácionária do número é: <strong>" . number_format($fra, 3, ",", ".") . "</strong>";
         ?>
     </section>
 
